af820f9a7a77cf2400432ad5c85a9fa4804df3f3
hijas <hijasahamed2000@gmail.com>
2025-01-25 18:50:50 +0530

    fix: weather loader circluar indicator color changed

--- lib/viewmodel/weather_controller.dart
+++ lib/viewmodel/weather_controller.dart
@@ -55,7 +55,7 @@ class WeatherController extends GetxController {
 
   Widget buildWeatherInfo() {
     if (isLoading.value) {
-      return Center(child: CircularProgressIndicator());
+      return Center(child: CircularProgressIndicator(color: Colors.white,));
     }
 
     if (errorMessage.isNotEmpty) {
